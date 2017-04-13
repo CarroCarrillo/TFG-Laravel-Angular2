1bc25f30052a73860e1d7a2d894ea8625ccee3fc
Daniel Carrillo Martínez <dcm3651@gmail.com>
2017-04-13 14:08:56 +0200

    Panel de inicio

--- xampp/htdocs/tfg/public/tfg/src/app/components/headers/header-simple.component.ts
+++ xampp/htdocs/tfg/public/tfg/src/app/components/headers/header-simple.component.ts
@@ -9,13 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderSimpleComponent implements OnInit {
     open: boolean;
 
-    constructor() { }
+    constructor() {
+        document.onclick = (e) => {
+            console.log(this.open);
+            console.log("Debería hacer algo");
+            if (e.target == document.getElementById('login-menu') || !this.isChildOf(e.target, document.getElementById('login-menu'))) {
+                this.open = false;
+                console.log("Debería cerrarse");
+            }
+            console.log(this.open);
+        }
+    }
 
-    ngOnInit() { 
+    ngOnInit() {
         this.open = false;
     }
 
-    openLogin(){
+    openLogin() {
+        console.log(this.open);
         this.open = true;
+    }
+
+    isChildOf(child, parent) {
+        if (child.parentNode === parent) {
+            return true;
+        } else if (child.parentNode === null) {
+            return false;
+        } else {
+            return this.isChildOf(child.parentNode, parent);
+        }
     }
 }
