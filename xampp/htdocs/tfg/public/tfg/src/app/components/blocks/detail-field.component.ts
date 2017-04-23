61dd432792ed9ed59783e16fa14d35b9ece47c50
Daniel Carrillo Martínez <dcm3651@gmail.com>
2017-04-23 13:28:18 +0200

    Estilo detalla y detail-field

--- xampp/htdocs/tfg/public/tfg/src/app/components/blocks/detail-field.component.ts
+++ xampp/htdocs/tfg/public/tfg/src/app/components/blocks/detail-field.component.ts
@@ -8,12 +8,6 @@ import { Component, Input } from '@angular/core';
 export class DetailFieldComponent {
   @Input() title: string;
   @Input() value: any;
-  @Input() node: Node;
-  @Input() maxlength: number;
-  @Input() edit: boolean;
-  @Input() property: string;
-  @Input() extended: string;
-  @Input() notmodify: boolean; //Indica que el campo no se puede modificar en el modo edición
   @Input() link: string; //Indica si el campo es un enlace y hacia dónde apunta
 
   constructor(){}
