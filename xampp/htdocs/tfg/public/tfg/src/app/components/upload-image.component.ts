f1baeac223656aa8cfbee68bb1b706d13e37ae79
Daniel Carrillo Martínez <dcm3651@gmail.com>
2017-04-27 16:00:10 +0200

    FormBuilder

--- xampp/htdocs/tfg/public/tfg/src/app/components/upload-image.component.ts
+++ xampp/htdocs/tfg/public/tfg/src/app/components/upload-image.component.ts
@@ -2,36 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { ApiService } from 'app/services/api.service';
 import { Image } from '../models/image';
+import '../../../node_modules/tags-input/tags-input.js';
 
 @Component({
     selector: 'upload-image',
     templateUrl: '../templates/upload-image.component.html',
-    styleUrls: ['../css/upload-image.component.css']
+    styleUrls: ['../css/upload-image.component.css', '../../../node_modules/tags-input/tags-input.css']
 })
 
 export class UploadImageComponent implements OnInit {
-    image: Image;
+    image: Image = new Image();
     public imageForm = this.fb.group({
-        contributor: [""],
-        coverage: [""],
-        created_at: [""],
-        creator: [""],
-        date: [""],
-        description: [""],
-        format: [""],
-        hashedName: [""],
-        id: [""],
-        identifier: [""],
-        language: [""],
-        publisher: [""],
-        relation: [""],
-        rights: [""],
-        source: [""],
-        subject: [""],
-        title: [""],
-        type: [""],
-        updated_at: [""],
-        filename: [""]
+        contributor: [this.image.contributor, ""],
+        coverage: [this.image.coverage, ""],
+        creator: [this.image.creator, ""],
+        date: [this.image.date, ""],
+        description: [this.image.description, ""],
+        format: [this.image.format, ""],
+        identifier: [this.image.identifier, ""],
+        language: [this.image.language, ""],
+        publisher: [this.image.publisher, ""],
+        relation: [this.image.relation, ""],
+        rights: [this.image.rights, ""],
+        source: [this.image.source, ""],
+        subject: [this.image.subject, ""],
+        title: [this.image.title, ""],
+        type: [this.image.type, ""]
     });
 
     constructor(private apiService: ApiService, private fb: FormBuilder) { }
@@ -47,5 +43,9 @@ export class UploadImageComponent implements OnInit {
         };
 
         fr.readAsDataURL(image);
+    }
+
+    onSubmit(){
+        console.log(this.image);
     }
 }
